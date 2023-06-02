c9eadda4b0f07d90fec8441a42a750fe5e1bfe5a
Juan Guillermo Buitrago Calle <64021772+Juanbc2@users.noreply.github.com>
2023-06-02 11:47:40 -0500

    actualizada consulta

--- searchneo4j/views.py
+++ searchneo4j/views.py
@@ -45,37 +45,124 @@ def about(request):
 # article.bibo__shortDescription AS Palabras_Clave,article.dct__created[0] as Fecha_de_publicación,article.bibo__abstract[0] as Abstract ORDER BY article.dct__created[0] DESC Limit 100
 
 def paper_list(request):
-    print("Request: ")
+    PMCIDS = ["32837492",
+      "32705994",
+      "33032629",
+      "32549076",
+      "32569835",
+      "32773850",
+      "32677557",
+      "32792322",
+      "32989380",
+      "33287080",
+      "32843866",
+      "33493188",
+      "33163146",
+      "32638025",
+      "32912768",
+      "33240837",
+      "32680889",
+      "29955859",
+      "32363138",
+      "32882347",
+      "32536944",
+      "32840492",
+      "32247928",
+      "32705485",
+      "32780574",
+      "32439956",
+      "33384521",
+      "32574184",
+      "33376490",
+      "32613654",
+      "32835684",
+      "32988991",
+      "33274726",
+      "32171058",
+      "33013254",
+      "33505860",
+      "32410245",
+      "15717217",
+      "12757561",
+      "32963508",
+      "32838031",
+      "33144275",
+      "33519017",
+      "33255672",
+      "32834884",
+      "33234656",
+      "32257511",
+      "32593116",
+      "32488583",
+      "33464206",
+      "32830471",
+      "32327946",
+      "32834655",
+      "33643929",
+      "32769754",
+      "33318721",
+      "32412403",
+      "32780149",
+      "33212953",
+      "32646656",
+      "32305069",
+      "32986741",
+      "33063226",
+      "32655724",
+      "32580969",
+      "33510633",
+      "30935725",
+      "33603709",
+      "33606157",
+      "33250552",
+      "33163951",
+      "33594628",
+      "33001869",
+      "32287412",
+      "33244357",
+      "32837287",
+      "16028157",
+      "32943211",
+      "32967646",
+      "22000601",
+      "32616970",
+      "33101676",
+      "33088099",
+      "33257496",
+      "32795831",
+      "33442303",
+      "33352802",
+      "32891255",
+      "33318716",
+      "32821920",
+      "33041374",
+      "33246864",
+      "32913284",
+      "33317745",
+      "33603349",
+      "32838064",
+      "32445083",
+      "30573330",
+      "23810340",
+      "33162696"]
     ontologies = ['SNOMEDCT', 'OCHV', 'PREMEDONTO', 'NCIT', 'IOBC', 'HL7', 'COVID-19', 'MESH', 'CIDO', 'CODO', 'HPIO', 'IDO-COVID-19', 'VO', 'COVIDCRFRAPID', 'BAO']
-    ontology = request.GET.get('ontology')
-    if not ontology or ontology == '' or ontology == 'ALL':
-        n = rd.randint(0, len(ontologies) - 1)
-        ontology = ontologies[n]
-    limit = request.GET.get('limit')
-    if not limit or limit == '':
-        limit = rd.randint(150, 170)
-    # Consulta Neo4j para recuperar todos los nodos de tipo Paper
-    query = "MATCH (article:bibo__Document)<-[:aoc__annotatesResource]-(concept:aot__ExactQualifier)-[:aoc__hasTopic]->(ontology:n4sch__Class) \
-            MATCH (author:foaf__Person)<-[]-(:rdf__Seq)<-[:bibo__authorList]-(article)-[:dct__publisher]->(organization:foaf__Organization) \
-            WHERE ontology.dct__source[0] ='" + ontology + "' WITH collect(distinct author.foaf__name[0]) AS Autores,article,ontology,organization \
-            RETURN DISTINCT article.bibo__pmid[0] AS PMC_ID, article.dct__title[0] AS Título,article.dct__description[0] as Tipo,ontology.dct__source[0] AS Ontología,Autores,organization.foaf__name[0] \
-            AS Organización,article.bibo__shortDescription AS Palabras_Clave,article.dct__created[0] as Fecha_de_publicación,article.bibo__abstract[0] as Abstract LIMIT " + str(limit)
-    results, meta = db.cypher_query(query)
-    
-    papers = [{'id': rec[0],
-                'title': rec[1],
-                'subtitle': 'This is the subtitle',
-                'article_type': rec[2],
-                'ontology': rec[3],
-                'authors': [{'full_name': author} for author in rec[4]],
-                'organization': rec[5],
-                'key_words': rec[6],
-                'published_date': rec[7],
-                'published_year': (rec[7].split(' ')[0]).split('-')[2] if (rec[7] and len(rec[7]) == 19) else None,
-                'abstract': rec[8],
-              } for rec in results]
-
-    """ return render(request, 'paper_list.html', {'papers': papers}) """
+    papers = []
+    for pmcid in PMCIDS:
+        query = "MATCH (article:bibo__Document)<-[:aoc__annotatesResource]-()-[:aoc__hasTopic]->(ontology:n4sch__Class) MATCH (author:foaf__Person)-[:foaf__publications]->(article)-[:dct__publisher]->(organization:foaf__Organization) WHERE article.bibo__pmid[0] = '"+pmcid+"' WITH collect(DISTINCT author.foaf__name[0]) AS Autores,article,collect(distinct ontology.dct__source[0]) AS Ontología,organization RETURN DISTINCT article.bibo__pmid[0] AS PMC_ID,article.bibo__doi[0] AS DOI,article.dct__title[0] AS Título,article.bibo__abstract[0] as Abstract,article.dct__description[0] as Tipo,Ontología,Autores,organization.foaf__name[0] AS Organización,article.dct__created[0] as Fecha_de_publicación"
+        rec, meta = db.cypher_query(query)
+        #PMC_ID	DOI	Título	Abstract	Tipo	Ontología	Autores	Organización	Fecha_de_publicación
+        rec = rec[0]
+        paper = {'pmcid': rec[0],
+                'doi': rec[1],
+                'title': rec[2],
+                'abstract': rec[3],
+                'paper_type': rec[4],
+                'ontologies': rec[5],
+               'authors': [{'full_name': author} for author in rec[6]],
+                'journal': rec[7],
+                'published_date': (rec[7].split(' ')[0]) if (rec[7] and len(rec[7]) == 19) else '2010-01-01',
+              }
+        papers.append(paper)
     return JsonResponse(papers, safe=False)
 
 def viewsPMCID (request):
